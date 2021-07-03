b1ad46a769f6156c68248f8b61cc139a71191064
Naoto Inoue <dorapocket22@gmail.com>
2021-07-03 21:11:42 +0900

    modified

--- exercise-1/ex1.21.scm
+++ exercise-1/ex1.21.scm
@@ -11,6 +11,6 @@
 (define (divides? a b)
   (= (remainder b a) 0))
 
-(smallest-divisor 199)   ; => 199
-(smallest-divisor 1999)  ; => 1999
-(smallest-divisor 19999) ; => 7
+(smallest-divisor 199)   ;; => 199
+(smallest-divisor 1999)  ;; => 1999
+(smallest-divisor 19999) ;; => 7
