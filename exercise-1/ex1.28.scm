723891eaeb500b319d31ab74b81992a18da810c4
Naoto Inoue <dorapocket22@gmail.com>
2021-08-01 21:24:22 +0900

    Update 1.28

--- exercise-1/ex1.28.scm
+++ exercise-1/ex1.28.scm
@@ -1,40 +1,33 @@
-;; ex1.28.scm
-;; 未完
-
 ;; Miller-Rabin test
-
-(define (square x) (* x x))
+(use srfi-27)
 
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp)
-	 (remainder (square (expmod base (/ exp 2) m))
-		    m))
-	(else
-	 (remainder (* base (expmod base (- exp 1) m))
-		    m))))
+	       (remainder (non-trivial-square-check (expmod base (/ exp 2) m))
+		                m))
+	      (else
+	       (remainder (* base (expmod base (- exp 1) m))
+		                m))))
 
 (define (miller-rabin-test n)
   (define (try-it a)
     (= (expmod a (- n 1) n) 1))
-  (define (check a)
-    (if (= a 1)
-	#t
-	(and (try-it a)
-	     (check (- a 1)))))
-  (check (- n 1)))
+  (try-it (+ 1 (random-integer (- n 1)))))
 
-(display "miller-rabin test")
-(newline)
-(display "Carmichael numbers:")
-(newline)
-(display "561,1105,1729,2465,2821,6601")
-(newline)
-(display (miller-rabin-test 561))
-(display (miller-rabin-test 1105))
-(display (miller-rabin-test 1729))
-(display (miller-rabin-test 2465))
-(display (miller-rabin-test 2821))
-(display (miller-rabin-test 6601))
-(display (miller-rabin-test 341550071728321))
-(newline)
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((miller-rabin-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(define (prime? n)
+  (fast-prime? n 10))
+
+(prime? 13)
+(prime? 561)
+(prime? 1105)
+(prime? 1729)
+(prime? 2465)
+(prime? 2821)
+(prime? 6601)
+(prime? 341550071728321)
