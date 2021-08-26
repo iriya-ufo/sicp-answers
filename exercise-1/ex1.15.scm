0ae6d44215aab0c9eb491470218d729f02c7e101
Naoto Inoue <dorapocket22@gmail.com>
2021-08-26 14:24:24 +0900

    Fix ex1.15

--- exercise-1/ex1.15.scm
+++ exercise-1/ex1.15.scm
@@ -1,3 +1,6 @@
+(use slib)
+(require 'trace)
+
 (define (cube x) (* x x x))
 
 (define (p x) (- (* 3 x) (* 4 (cube x))))
@@ -7,4 +10,23 @@
       angle
       (p (sine (/ angle 3.0)))))
 
+(trace sine)
 (sine 12.15)
+;; CALL sine 12.15
+;;   CALL sine 4.05
+;;     CALL sine 1.3499999999999999
+;;       CALL sine 0.44999999999999996
+;;         CALL sine 0.15
+;;         RETN sine 0.1495
+;;       RETN sine 0.4351345505
+;;     RETN sine 0.9758465331678772
+;;   RETN sine -0.7895631144708228
+;; RETN sine -0.39980345741334
+
+;; => -0.39980345741334
+
+;; a. trace の結果よりCALLされた回数なので5回
+;; b. Θ (log(a))
+
+;; 詳しい解説は以下を参照。
+;; https://www.notion.so/1-15-ae116b9deaef498990722ffb5e7e0da9
