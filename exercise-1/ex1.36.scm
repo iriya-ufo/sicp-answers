bde34c4219d30f51ec303062c1f323fddff65208
Naoto Inoue <dorapocket22@gmail.com>
2021-09-18 22:34:06 +0900

    Fix 1.36

--- exercise-1/ex1.36.scm
+++ exercise-1/ex1.36.scm
@@ -1,33 +1,20 @@
-;; ex1.36.scm
-
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2)) tolerance))
-  (define (try guess)
+  (define (try guess count)
     (let ((next (f guess)))
-      (cond ((close-enough? guess next)
-	     (print next))
-	    (else (print guess)
-		  (try next)))))
-  (try first-guess))
+      (cond ((close-enough? guess next) next)
+            (else (print count ": " guess)
+                  (try next (+ count 1))))))
+  (try first-guess 0))
 
-
-;; non-average-damping
-
-(print "non-average-damping")
-
+;; Non Average Damping
 (fixed-point (lambda (x) (/ (log 1000) (log x)))
-	     1.01)
-(newline)
+             1.01)
 
-
-;; average-damping
-
-(print "average-damping")
-
+;; Average Damping
 (define (average x y) (/ (+ x y) 2))
-
 (fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
-	     1.01)
+             1.01)
