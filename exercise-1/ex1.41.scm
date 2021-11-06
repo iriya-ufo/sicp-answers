51c4eb601491a4927d4f3d3e199e15aaf26232dc
Naoto Inoue <dorapocket22@gmail.com>
2021-11-07 00:13:24 +0900

    Added 40 and 41

--- exercise-1/ex1.41.scm
+++ exercise-1/ex1.41.scm
@@ -1,15 +1,18 @@
-;; ex1.41.scm
-
-;; doubleは引数として一引数の手続きを取り、
-;; 受け取った手続きを二回作用させる手続きを返す
-
 (define (double f)
   (lambda (x) (f (f x))))
 
 (define (inc x) (+ x 1))
 (define (square x) (* x x))
 
-(print ((double inc) 2))
-(print ((double square) 2))
+((double inc) 2)
+((double square) 2)
+(((double (double double)) inc) 5)      ; => 21
+(((d1 (d2 d3)) inc) 5)
+(((d2 d3) ((d2 d3) inc)) 5)
+(((d2 d3) (d3 (d3 inc))) 5)
+((d3 (d3 (d3 (d3 inc)))) 5)
 
-(print (((double (double double)) inc) 5))
+(((double (double double)) inc) 5)
+(((double double) ((double double) inc)) 5)
+(((double double) (double (double inc))) 5)
+((double (double (double (double inc)))) 5)
