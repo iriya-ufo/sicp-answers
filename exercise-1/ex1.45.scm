096415acf4ec063d3f86f008d6055b4c0af8490a
Naoto Inoue <dorapocket22@gmail.com>
2021-11-27 19:06:01 +0900

    wip

--- exercise-1/ex1.45.scm
+++ exercise-1/ex1.45.scm
@@ -1,5 +1,3 @@
-;; ex1.45.scm
-
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
@@ -8,8 +6,8 @@
   (define (try guess)
     (let ((next (f guess)))
       (if (close-enough? guess next)
-	  next
-	  (try next))))
+          next
+          (try next))))
   (try first-guess))
 
 (define (average x y) (/ (+ x y) 2))
@@ -22,49 +20,43 @@
       identity
       (compose f (repeated f (- n 1)))))
 
-
 ;; x の n 乗根を m 回の平均緩和を利用して解く
-
-(define (n-root-test m n x)
+(define (n-root-test x n m)
   (fixed-point ((repeated average-damp m)
-		(lambda (y) (/ x (expt y (- n 1)))))
-	       1.0))
+                (lambda (y) (/ x (expt y (- n 1)))))
+               1.0))
 
-;; 1回の平均緩和で収束しない
+;; 2乗根が何回で収束するか調べる
+(n-root-test 9 2 1)                     ; 1回で収束
+(n-root-test 16 2 1)                    ; 1回で収束
 
-;; (print (n-root-test 1 4 2)) ;;収束しない
+;; 3乗根が何回で収束するか調べる
+(n-root-test 27 3 1)                    ; 1回で収束
+(n-root-test 125 3 1)                   ; 1回で収束
 
-(print (n-root-test 2 4 2)) ;;収束
-(newline)
+;; 4乗根が何回で収束するか調べる
+(n-root-test 16 4 1)                    ; 1回で収束しない
+(n-root-test 16 4 2)                    ; 2回で収束
 
-(print (n-root-test 2 5 2)) ;;収束
-(newline)
+;; 5乗根が何回で収束するか調べる
+(n-root-test 32 5 1)                    ; 1回で収束しない
+(n-root-test 32 5 2)                    ; 2回で収束
 
-(print (n-root-test 2 6 2)) ;;収束
-(newline)
-
-(print (n-root-test 2 7 2)) ;;収束
-(newline)
 
 ;; 2回の平均緩和で収束しない
-
 ;; (print (n-root-test 2 8 2)) ;;収束しない
-
-(print (n-root-test 3 8 2)) ;;収束
-(newline)
-
+(n-root-test 3 8 2) ;;収束
 
 ;; n = 2^c とすると n 乗根を計算するのに必要な平均緩和の回数は c 回
-
 (define (n-root n x)
   (define (damp-count m)
     (if (< m 2)
-	0
-	(+ 1 (damp-count (/ m 2)))))
+        0
+        (+ 1 (damp-count (/ m 2)))))
   (fixed-point ((repeated average-damp (damp-count n))
-		(lambda (y) (/ x (expt y (- n 1)))))
-	       1.0))
+                (lambda (y) (/ x (expt y (- n 1)))))
+               1.0))
 
-(print (n-root 4 2))
-(print (n-root 16 2))
-(print (n-root 6 8))
+(n-root 4 2)
+(n-root 16 2)
+(n-root 6 8)
