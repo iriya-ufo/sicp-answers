d0165a5a1b334eaa8fed8cdbdf1f346192fed4da
Naoto Inoue <dorapocket22@gmail.com>
2021-12-04 19:28:10 +0900

    Done 1.46

--- exercise-1/ex1.46.scm
+++ exercise-1/ex1.46.scm
@@ -1,36 +1,36 @@
-;; ex1.46.scm
+(define (average x y)
+  (/ (+ x y) 2))
 
 (define (iterative-improve good-enough? improve)
   (lambda (guess)
     (if (good-enough? guess)
-	guess
-	((iterative-improve good-enough? improve) (improve guess)))))
+        guess
+        ((iterative-improve good-enough? improve)
+         (improve guess)))))
 
-(define (square x) (* x x))
-
-
-;; iterative-improveを用いたsqrt手続き
-
+;;; sqrt with iterative-improve
 (define (sqrt x)
   (define (good-enough? guess)
-    (< (abs (- (square guess) x)) 0.00001))
+    (< (abs (- (square guess) x)) 0.001))
   (define (improve guess)
-    (define (average x y) (/ (+ x y) 2))
     (average guess (/ x guess)))
   ((iterative-improve good-enough? improve) 1.0))
 
-(print (sqrt 2))
-(print (sqrt 3))
-(newline)
+(sqrt 3)
+;; => 1.7321428571428572
 
+(sqrt 5)
+;; => 2.2360688956433634
 
-;; iterative-improveを用いたfixed-point手続き
-
+;;; fixed-point with iterative-improve
 (define (fixed-point f first-guess)
   (define (close-enough? guess)
-    (< (abs (- guess (f guess))) 0.00001))
+    (< (abs (- guess (f guess))) 0.0001))
   ((iterative-improve close-enough? f) first-guess))
-  
-(print (fixed-point cos 1.0))
-(print (fixed-point (lambda (y) (+ (sin y) (cos y)))
-		    1.0))
+
+(fixed-point cos 1.0)
+;; => 0.7391301765296711
+
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+;; => 1.2587758014705526
