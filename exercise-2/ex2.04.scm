a08f527880f4c856922f23641213c569ad2d24a9
Naoto Inoue <dorapocket22@gmail.com>
2022-01-23 20:05:00 +0900

    Done 2.04 and 2.05

--- exercise-2/ex2.04.scm
+++ exercise-2/ex2.04.scm
@@ -1,27 +1,22 @@
-;; ex2.04.scm
-
 (define (cons x y)
   (lambda (m) (m x y)))
 
 (define (car z)
   (z (lambda (p q) p)))
 
-
 ;; 置換えモデル
-
 ;; (car (cons x y))
 ;; ((cons x y) (lambda (p q) p))
 ;; ((lambda (m) (m x y)) (lambda (p q) p))
 ;; ((lambda (p q) p) x y)
 ;; x
 
-
-;; cdrの定義
-
+;; cdr の定義
 (define (cdr z)
   (z (lambda (p q) q)))
 
-
 (define foo (cons 1 2))
-(print (car foo))
-(print (cdr foo))
+(car foo)
+;; => 1
+(cdr foo)
+;; => 2
--- exercise-2/ex2.05.scm
+++ exercise-2/ex2.05.scm
@@ -1,25 +1,18 @@
-;; ex2.05.scm
-
-(define (my-cons a b)
+(define (cons a b)
   (* (expt 2 a) (expt 3 b)))
 
-(define (my-car z)
+(define (car z)
   (if (even? z)
-      (+ 1 (my-car (/ z 2)))
+      (+ 1 (car (/ z 2)))
       0))
 
-(define (my-cdr z)
+(define (cdr z)
   (if (= 0 (remainder z 3))
-      (+ 1 (my-cdr (/ z 3)))
+      (+ 1 (cdr (/ z 3)))
       0))
 
-(define foo (my-cons 5 3))
-(print foo)
-(print (my-car foo))
-(print (my-cdr foo))
-(newline)
-
-(define bar (my-cons 0 8))
-(print bar)
-(print (my-car bar))
-(print (my-cdr bar))
+(define foo (cons 5 3))
+(car foo)
+;; => 5
+(cdr foo)
+;; => 3
