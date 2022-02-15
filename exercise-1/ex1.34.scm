ba616d07fbb1d14275eb49a014597496647e0212
Naoto Inoue <dorapocket22@gmail.com>
2022-02-15 12:59:50 +0900

    Done 1.34

--- exercise-1/ex1.34.scm
+++ exercise-1/ex1.34.scm
@@ -1,15 +1,15 @@
-;; ex1.34.scm
-
 (define (f g)
   (g 2))
 
 (define (square x) (* x x))
 
-(print (f square))
+(f square)
+;; => 4
 
-(print (f (lambda (z) (* z (+ z 1)))))
+(f (lambda (z) (* z (+ z 1))))
+;; => 6
 
-(print (f f))
+(f f)
+;; *** ERROR: invalid application: (2 2)
 
-;; (f f)を評価するとまず(f 2)となり、
-;; さらにこれを評価すると(2 2)となってしまい引数に作用させることができなくなる。
+;; (f f) を評価するとまず (f 2) となり、さらにこれを評価すると (2 2) となってしまい引数に作用させることができなくなる
