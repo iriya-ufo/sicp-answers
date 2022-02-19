81be4d0ce8c697adef6c659a60dc1b0893b6a21f
Naoto Inoue <dorapocket22@gmail.com>
2022-02-19 19:40:05 +0900

    Done 2.12

--- exercise-2/ex2.12.scm
+++ exercise-2/ex2.12.scm
@@ -1,13 +1,9 @@
-;; ex2.12.scm
-;; 未完
-
 (define (make-interval a b) (cons a b))
 (define (upper-bound x) (cdr x))
 (define (lower-bound x) (car x))
 
 (define (make-center-width c w)
   (make-interval (- c w) (+ c w)))
-
 (define (center i)
   (/ (+ (lower-bound i) (upper-bound i)) 2))
 (define (width i)
@@ -15,6 +11,5 @@
 
 (define (make-center-percent c p)
   (make-center-width c (/ (* c p) 100.0)))
-
 (define (percent i)
   (* (/ (width i) (center i)) 100.0))
