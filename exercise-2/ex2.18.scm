0fb1c74e904d275c04efc70fff9f63ea4b03ee05
Naoto Inoue <dorapocket22@gmail.com>
2022-03-19 19:39:15 +0900

    Done 2.18

--- exercise-2/ex2.18.scm
+++ exercise-2/ex2.18.scm
@@ -1,10 +1,9 @@
-;; ex2.18.scm
-
 (define (reverse list)
   (define (reverse-iter rev-list list)
     (if (null? list)
-	rev-list
-	(reverse-iter (cons (car list) rev-list) (cdr list))))
+        rev-list
+        (reverse-iter (cons (car list) rev-list) (cdr list))))
   (reverse-iter '() list))
 
 (reverse (list 1 4 9 16 25))
+;; => (25 16 9 4 1)
