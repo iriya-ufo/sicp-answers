d7713bbe3d8f36313ea6b4f1fe890232b14251e0
Naoto Inoue <dorapocket22@gmail.com>
2022-03-19 18:15:40 +0900

    Done 2.17

--- exercise-2/ex2.17.scm
+++ exercise-2/ex2.17.scm
@@ -1,10 +1,9 @@
-;; ex2.17.scm
-
 (define (last-pair list)
   (define (last-pair-iter cdr-list list)
     (if (null? cdr-list)
-	list
-	(last-pair-iter (cdr cdr-list) (cdr list))))
+        list
+        (last-pair-iter (cdr cdr-list) (cdr list))))
   (last-pair-iter (cdr list) list))
 
 (last-pair (list 23 72 149 34))
+;; => (34)
