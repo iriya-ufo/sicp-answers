597a1e4eac870e6655a60179d3d8162121daffb6
Naoto Inoue <dorapocket22@gmail.com>
2022-04-02 19:51:01 +0900

    Done 2.20

--- exercise-2/ex2.20.scm
+++ exercise-2/ex2.20.scm
@@ -1,32 +1,16 @@
-;; ex2.20.scm
-
-;; ドット末尾記法
-
-;; (define (f x y . z) <body>)
-;; (define (g . w) <body>)
-
-
-;; lambda-version
-
-;; (define f (lambda (x y . z) <body>))
-;; (define g (lambda w <body>))
-
-
-(define (f x y . z) (+ x y (car z)))
-(define (g . w) (cdr w))
-
-(f 1 2 3 4 5 6)
-(g 1 2 3 4 5 6)
-
-
 (define (same-parity x . z)
   (define even-odd-check (if (even? x) even? odd?))
   (define (filter check z)
     (cond ((null? z) z)
-	  ((check (car z)) (cons (car z) (filter check (cdr z))))
-	  (else (filter check (cdr z)))))
+          ((check (car z)) (cons (car z) (filter check (cdr z))))
+          (else (filter check (cdr z)))))
   (cons x (filter even-odd-check z)))
 
 (same-parity 1 2 3 4 5 6 7)
+;; => (1 3 5 7)
+
 (same-parity 2 3 4 5 6 7)
+;; => (2 4 6)
+
 (same-parity 2)
+;; => (2)
