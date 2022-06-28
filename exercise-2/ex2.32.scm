56b73557bd46d38d1a95c81b37e208d97bd7419e
Naoto Inoue <dorapocket22@gmail.com>
2022-06-28 22:04:05 +0900

    Done 2.32

--- exercise-2/ex2.32.scm
+++ exercise-2/ex2.32.scm
@@ -1,10 +1,30 @@
-;; ex2.32.scm
-;; 未完
-
 (define (subsets s)
   (if (null? s)
       (list '())
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (x) (cons (car s) x)) rest)))))
 
-(print (subsets (list 1 2 3)))
+(subsets '(1 2 3))
+
+(define ls '(() (3)))
+ls
+(map f rest)
+(map f ls)
+
+rest_3 = '(() (3))
+(define rest_3 '(() (3)))
+
+(cons 2 '())                            ; => (2)
+(cons 2 '(3))                           ; => (2 3)
+
+(append rest_3 '((2) (2 3)))            ; => (() (3) (2) (2 3))
+
+rest_2 = '(() (3) (2) (2 3))
+(define rest_2 '(() (3) (2) (2 3)))
+
+(cons 1 '())                            ; => (1)
+(cons 1 '(3))                           ; => (1 3)
+(cons 1 '(2))                           ; => (1 2)
+(cons 1 '(2 3))                         ; => (1 2 3)
+
+(append rest_2 '((1) (1 3) (1 2) (1 2 3))) ; => (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
