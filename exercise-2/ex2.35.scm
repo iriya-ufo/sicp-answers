3dbdeba23f023b20f85c9f3be81b3d62e73edaa2
Naoto Inoue <dorapocket22@gmail.com>
2022-07-12 22:19:34 +0900

    Done 2.35

--- exercise-2/ex2.35.scm
+++ exercise-2/ex2.35.scm
@@ -1,5 +1,3 @@
-;; ex2.35.scm
-
 (define a-list (list 1 2 3 4))
 (define b-list (list 5 6 7 8))
 
@@ -9,21 +7,24 @@
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
 
-;; 2.2.2節の count-leaves
-(define (count-leaves2.2.2 x)
+;;; 2.2.2 節の count-leaves
+(define (count-leaves-2.2.2 x)
   (cond ((null? x) 0)
-	((not (pair? x)) 1)
-	(else (+ (count-leaves2.2.2 (car x))
-		 (count-leaves2.2.2 (cdr x))))))
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves-2.2.2 (car x))
+                 (count-leaves-2.2.2 (cdr x))))))
 
-(count-leaves2.2.2 a-list)
-(count-leaves2.2.2 (cons a-list b-list))
-(count-leaves2.2.2 (append a-list b-list))
+(count-leaves-2.2.2 a-list)                             ; => 4
+(count-leaves-2.2.2 (cons a-list b-list))               ; => 8
+(count-leaves-2.2.2 (cons a-list (list a-list b-list))) ; => 12
 
-;; 再定義
+;;; 再定義
 (define (count-leaves t)
-  (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1)) t)))
+  (accumulate + 0 (map (lambda (x) (if (pair? x)
+                                       (count-leaves x)
+                                       1))
+                       t)))
 
 (count-leaves a-list)
 (count-leaves (cons a-list b-list))
-(count-leaves (append a-list b-list))
+(count-leaves (cons a-list (list a-list b-list)))
