eb909aee383c7cba11581879a605e046992c92cb
Naoto Inoue <dorapocket22@gmail.com>
2022-07-12 20:56:51 +0900

    Done 2.34

--- exercise-2/ex2.34.scm
+++ exercise-2/ex2.34.scm
@@ -1,16 +1,14 @@
-;; ex2.34.scm
-
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
 
-;; Horner's rule
+;;; Horner's rule
 (define (horner-eval x coefficient-sequence)
   (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* higher-terms x)))
               0
               coefficient-sequence))
 
-;; x = 2 で 1 + 3x + 5x^3 + x^5 の計算
+;;; x = 2 で 1 + 3x + 5x^3 + x^5 の計算
 (horner-eval 2 (list 1 3 0 5 0 1))
