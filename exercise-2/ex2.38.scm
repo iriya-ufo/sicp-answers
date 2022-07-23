86ecda6c978877a47e71a8d5e3ec3dc2dda08473
Naoto Inoue <dorapocket22@gmail.com>
2022-07-23 18:30:01 +0900

    Done 2.38

--- exercise-2/ex2.38.scm
+++ exercise-2/ex2.38.scm
@@ -1,24 +1,20 @@
-;; ex2.38.scm
-
 (define (fold-left op initial sequence)
   (define (iter result rest)
     (if (null? rest)
-	result
-	(iter (op result (car rest))
-	      (cdr rest))))
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
   (iter initial sequence))
 
-(fold-right / 1 '(1 2 3))
-(fold-left / 1 '(1 2 3))
+(fold-right / 1 '(1 2 3))               ; => 3/2
+(fold-left / 1 '(1 2 3))                ; => 1/6
+(fold-right list '() '(1 2 3))          ; => (1 (2 (3 ())))
+(fold-left list '() '(1 2 3))           ; => (((() 1) 2) 3)
+(fold-right - 0 '(9 8 7))               ; => 8
+(fold-left - 0 '(9 8 7))                ; => -24
+(fold-right * 1 '(1 2 3))               ; => 6
+(fold-left * 1 '(1 2 3))                ; => 6
+(fold-right + 1 '(1 2 3))               ; => 7
+(fold-left + 1 '(1 2 3))                ; => 7
 
-(fold-right list '() '(1 2 3))
-(fold-left list '() '(1 2 3))
-
-(fold-right - 0 '(9 8 7))
-(fold-left - 0 '(9 8 7))
-
-
-;; 結合法則を満たすこと
-;; より形式的には、2項演算「・」が定義された集合Sにおいて、Sの任意の元
-;; a,b,cに対し次の等式が成り立てば、2項演算・は結合法則を満たすという
-;; a・(b・c)=(a・b)・c
+;; 交換法則が成り立つこと
