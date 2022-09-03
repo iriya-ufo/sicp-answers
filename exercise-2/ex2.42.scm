1d315559f28aea371afbd51b5ec676eb60dccbbb
Naoto Inoue <dorapocket22@gmail.com>
2022-09-03 20:14:07 +0900

    WIP 2.42 queen-puzzle

---
+++ exercise-2/ex2.42.scm
@@ -0,0 +1,14 @@
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (iota board-size 1)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
