fa1bc79f6d6db6eccdda858a813bb14a54ce0beb
Naoto Inoue <dorapocket22@gmail.com>
2022-11-10 22:14:48 +0900

    Done 2.48 and wip 2.49

---
+++ exercise-2/ex2.49.scm
@@ -0,0 +1,22 @@
+;; a.
+(define outline-painter-segment-list
+  (list (make-segment (make-vect 0 0) (make-vect 1 0))
+        (make-segment (make-vect 0 0) (make-vect 0 1))
+        (make-segment (make-vect 1 0) (make-vect 1 1))
+        (make-segment (make-vect 0 1) (make-vect 1 1))))
+
+(define outline-painter (segments->painter outline-painter-segment-list))
+
+;; b.
+(define x-painter-segment-list
+  (list (make-segment (make-vect 0 0) (make-vect 1 1))
+        (make-segment (make-vect 0 1) (make-vect 1 0))))
+
+;; c.
+(define rhombus-painter-segment-list
+  (list (make-segment (make-vect 0.5 0) (make-vect 1 0.5))
+        (make-segment (make-vect 1 0.5) (make-vect 0.5 1))
+        (make-segment (make-vect 0.5 1) (make-vect 0 0.5))
+        (make-segment (make-vect 0 0.5) (make-vect 0.5 0))))
+
+;; d.
