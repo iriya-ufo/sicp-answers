d452ec93e178ea6f6960a506a3f6f3fba7bcdd7a
Naoto Inoue <dorapocket22@gmail.com>
2023-01-18 20:59:49 +0900

    Done 2.59

---
+++ exercise-2/ex2.59.scm
@@ -0,0 +1,25 @@
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((not (element-of-set? (car set1) set2))
+         (cons (car set1) (union-set (cdr set1) set2)))
+        (else (union-set (cdr set1) set2))))
+
+(intersection-set '(1 2 3) '(2 3 4))    ; => (2 3)
+(union-set '(1 2 3) '(4 5 6))           ; => (1 2 3 4 5 6)
+(union-set '(1 2) '(2 3))               ; => (1 2 3)
