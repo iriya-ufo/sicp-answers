10916f9e5f16ee3b5012e8a1672a3503204c79ac
Naoto Inoue <dorapocket22@gmail.com>
2023-02-01 21:52:09 +0900

    Done 2.61

---
+++ exercise-2/ex2.61.scm
@@ -0,0 +1,11 @@
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< x (car set)) (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(adjoin-set 0 '(1 3 5 7 9))             ; => (0 1 3 5 7 9)
+(adjoin-set 4 '(1 3 5 7 9))             ; => (1 3 4 5 7 9)
+(adjoin-set 7 '(1 3 5 7 9))             ; => (1 3 5 7 9)
+(adjoin-set 10 '(1 3 5 7 9))            ; => (1 3 5 7 9 10)
