640da079504eb77210c3635714562762a4ce8241
Naoto Inoue <dorapocket22@gmail.com>
2023-05-31 23:02:35 +0900

    wip

---
+++ exercise-2/ex2.69.scm
@@ -0,0 +1,49 @@
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)   ; symbol
+                               (cadr pair)) ; frequency
+                    (make-leaf-set (cdr pairs))))))
+
+(define pairs '((A 4) (B 2) (C 1) (D 1)))
+;; (define pairs '((B 2) (A 4) (D 1) (C 1)))
+;; (define pairs '((C 1) (B 2) (A 4) (D 1)))
+
+(make-leaf-set pairs)
+;; => ((leaf D 1) (leaf C 1) (leaf B 2) (leaf A 4))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaf-set)
+  (if (= (length leaf-set) 1)
+      huffman-tree
+      ))
