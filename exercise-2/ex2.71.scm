b5f7419cf694a7abad4fd940a40f842f7232ed84
Naoto Inoue <dorapocket22@gmail.com>
2023-06-29 11:00:45 +0900

    WIP 2.71

--- exercise-2/ex2.71.scm
+++ exercise-2/ex2.71.scm
@@ -2,3 +2,7 @@
 ;; ((A 1) (B 2) (C 4) (D 8) (E 16))
 
 ;; ((A B C D E))
+
+(define pairs '((A 1) (B 2) (C 4) (D 8) (E 16)))
+(define pairs '((E 16) (D 8) (C 4) (B 2) (A 1)))
+(generate-huffman-tree pairs)
