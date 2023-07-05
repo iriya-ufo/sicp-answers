434b866a8ba53e9257299f9b104553b1707f9a6a
Naoto Inoue <dorapocket22@gmail.com>
2023-07-05 21:35:56 +0900

    Done 2.71

--- exercise-2/ex2.71.scm
+++ exercise-2/ex2.71.scm
@@ -6,3 +6,9 @@
 (define pairs '((A 1) (B 2) (C 4) (D 8) (E 16)))
 (define pairs '((E 16) (D 8) (C 4) (B 2) (A 1)))
 (generate-huffman-tree pairs)
+
+;; 最も頻度の高い記号を符号化するのに必要なビット数
+;; 1 bit
+
+;; 最も頻度の低い記号を符号化するのに必要なビット数
+;; n-1 bit
