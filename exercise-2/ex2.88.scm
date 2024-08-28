b488e8bf1d16f7ff912ed54c5e48cb02be6b80b2
Naoto Inoue <dorapocket22@gmail.com>
2024-08-28 21:29:35 +0900

    Done 2.88

--- exercise-2/ex2.88.scm
+++ exercise-2/ex2.88.scm
@@ -1,7 +1,16 @@
 ;;; 多項式のスカラー倍
 (define (scale-poly p x)
-  (map (lambda (term) (make-term (order term) (* x (coeff term))))
-       (term-list p)))
+  (make-poly
+   (variable p)
+   (scale-terms x (term-list p))))
+
+(define (scale-terms x L)
+  (if (empty-termlist? L)
+      (the-empty-termlist)
+      (let ((t (first-term L)))
+        (adjoin-term
+         (make-term (order t) (* x (coeff t)))
+         (scale-terms x (rest-terms L))))))
 
 ;;; 多項式の減算
 (define (sub-poly p1 p2)
